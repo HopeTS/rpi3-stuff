a588430068fb5b2665f50e7b7b1bded747cb8a6a
Robby Bergers <bergersr97@gmail.com>
2022-09-15 16:43:54 -0400

    remove function

--- blinking_led.py
+++ blinking_led.py
@@ -4,34 +4,24 @@ from time import sleep
 # This script is a basic test of GPIO, it makes an LED connected to GPIO14
 # and ground blink on and off for 10 seconds.
 
-# Configure GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(out_pin, GPIO.OUT, initial = GPIO.LOW)
 
-# Script vars
-out_pin = 8             # The pin sending the current
-current_pin_state = 0   # 0 = low, 1 = high
-maximum_cycles = 20     # number of times current alternates
 
-# Alternate the state of the pin
-def alternate_circuit():
-    # Turn current on
-    if (current_pin_state == 0):
-        print "Turning LED on..."
-        GPIO.output(out_pin, GPIO.HIGH)
-        current_pin_state = 1
-
-    # Turn current off
-    else:
-        print "Turning LED off..."
-        GPIO.output(out_pin, GPIO.LOW)
-        current_pin_state = 0
 
 
 
 
 
 if __name__ == "__main__":
+
+    # Configure GPIO
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(out_pin, GPIO.OUT, initial = GPIO.LOW)
+
+    # Script vars
+    out_pin = 8             # The pin sending the current
+    current_pin_state = 0   # 0 = low, 1 = high
+    maximum_cycles = 20     # number of times current alternates
+
     # Script output start
     print("LED will start blinking in ")
     i = 5
@@ -44,5 +34,18 @@ if __name__ == "__main__":
     for j in range(maximum_cycles):
         sleep(0.5)
         print "Cycle number:", i
-        alternate_circuit()
+        # Turn current on
+        if (current_pin_state == 0):
+            print "Turning LED on..."
+            GPIO.output(out_pin, GPIO.HIGH)
+            current_pin_state = 1
+
+        # Turn current off
+        else:
+            print "Turning LED off..."
+            GPIO.output(out_pin, GPIO.LOW)
+            current_pin_state = 0
+
         sleep(0.5)
+
+    print "Script complete."
