b36257bb700eefe8461815c36e39778667487f29
Robby Bergers <bergersr97@gmail.com>
2022-09-15 18:05:46 -0400

    minimal flask server to control GPIO pin remotely

--- blink_led_from_server/server/app.py
+++ blink_led_from_server/server/app.py
@@ -1,6 +1,30 @@
 from flask import Flask
+from time import sleep
+import RPi.GPIO as GPIO
 
+# Configure flask
 app = Flask(__name__)
+
+# Configure GPIO
+out_pin = 8                 # The pin sending the current
+current_pin_state = 0       # 0 = LOW, 1 = HIGH  
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(out_pin, GPIO.OUT, initial = GPIO.LOW)
+
+def led_blink()
+    for i in range(30):
+        if (current_pin_state = 0):
+            GPIO.output(out_pin, GPIO.HIGH)
+            current_pin_state = 1
+        
+        else:
+            GPIO.output(out_pin, GPIO.LOW)
+            current_pin_state = 0
+        
+        sleep(0.2)
+        
+
+    return
 
 @app.route("/")
 def hello_world():
